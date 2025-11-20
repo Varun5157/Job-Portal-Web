7d2d54828d0fbc1e9f394fbc3173e810add1f0c4
Varun5157 <k.v.kumar5157@gmail.com>
2025-11-20 16:53:44 +0530

    Initial commit with project files

---
+++ JobApp/src/main/java/com/varun/JobApp/controller/JobController.java
@@ -0,0 +1,45 @@
+package com.varun.JobApp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.varun.JobApp.model.JobPost;
+import com.varun.JobApp.service.JobService;
+
+@Controller
+public class JobController {
+
+	@Autowired
+	private JobService service;
+
+	@GetMapping({ "/", "home" })
+	public String home() {
+		return "home";
+	}
+
+	@GetMapping("addjob")
+	public String addJob() {
+		return "addjob";
+	}
+
+	@PostMapping("handleForm")
+	public String handleForm(JobPost jobPost) {
+		service.addJob(jobPost);
+		return "success";
+
+	}
+
+	@GetMapping("viewalljobs")
+	public String viewJobs(Model m) {
+		List<JobPost> jobs = service.getAllJobs();
+		m.addAttribute("jobPosts",jobs);
+		
+		return "viewalljobs";
+	}
+	
+}
